8422aaf900a32116738f10d6c045f80aaf2b5529
thibaultchazal <chazalthibault@gmail.com>
2018-03-18 18:06:06 +0100

    Add delete script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -10,16 +10,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, database) => {
 
   const db1 = database.db('TodoApp');
 
-  // db1.collection('Todos').find({
-  //   _id: new ObjectID('5aad3d5fd31fee67aff1957e')
-  // }).toArray().then((docs) => {
-  //   console.log('Todos');
-  //   console.log(JSON.stringify(docs, undefined, 2));
-  // }, (err) => {
-  //   console.log('Unable to fetch todos', err);
-  // });
   db1.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
       console.log(result);
    });
+   
   // database.close();
 });
